df7245d0b10975095ae502f8bfb4b7efd8e2cbc9
BarackOBusiness <BarackOBusiness@proton.me>
2026-05-14 14:21:30 -0400

    IL hooks moved here

---
+++ src/Hooks.cs
@@ -0,0 +1,152 @@
+using System.Collections.Generic;
+using System.Reflection;
+using BepInEx.Logging;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+
+namespace WideAngleCamera;
+
+internal static class Hooks {
+	private const BindingFlags Public = BindingFlags.Public;
+	private const BindingFlags Private = BindingFlags.NonPublic;
+	private const BindingFlags Static = BindingFlags.Static;
+	private const BindingFlags Instance = BindingFlags.Instance;
+
+	private static ManualLogSource _logger;
+	private static List<ILHook> _hooks;
+
+	internal static void Hook(BepInEx.Logging.ManualLogSource logger) {
+		_logger = logger;
+		_hooks = new List<ILHook>();
+		// Now start patching all the methods
+		_logger.LogInfo("Hooking `RenderTextureMain::Start`");
+		_hooks.Add(new ILHook(GetMethod<RenderTextureMain>("Start", Private | Instance), RenderTextureMain_Start));
+		_logger.LogInfo("Hooking `EnvironmentDirector::FogLogic`");
+		_hooks.Add(new ILHook(GetMethod<EnvironmentDirector>("FogLogic", Private | Instance), EnvironmentDirector_FogLogic));
+		_logger.LogInfo("Hooking `ValuableDiscoverGraphic::RendererBoundsInScreenSpace`");
+		_hooks.Add(new ILHook(GetMethod<ValuableDiscoverGraphic>("RendererBoundsInScreenSpace", Private | Instance), ValuableDiscoverGraphic_RendererBoundsInScreenSpace));
+		_logger.LogInfo("Hooking `SemiFunc::OnScreen`");
+		_hooks.Add(new ILHook(typeof(SemiFunc).GetMethod("OnScreen", Public | Static), SemiFunc_OnScreen));
+	}
+
+	internal static void Unhook() {
+		foreach (var hook in _hooks) {
+			hook.Dispose();
+		}
+	}
+
+	private static MethodInfo GetMethod<T>(string name, BindingFlags flags) {
+		return typeof(T).GetMethod(name, flags);
+	}
+
+	// Update camera list construction to account for all the new ones that shouldn't be appended
+	private static void RenderTextureMain_Start(ILContext il) {
+		ILCursor cursor = new ILCursor(il).Goto(0);
+
+		if (cursor.TryGotoNext(MoveType.After,
+			x => x.MatchBr(out _),
+			x => x.MatchLdloc(1),
+			x => x.MatchLdloc(2),
+			x => x.MatchLdelemRef(),
+			x => x.MatchStloc(3),
+			x => x.MatchLdarg(0)
+		)) {
+			cursor.RemoveRange(3);
+			cursor.Emit(OpCodes.Ldloc_3);
+			cursor.EmitDelegate((RenderTextureMain self, Camera cam) => {
+				if (cam.transform.parent != null && (cam.transform.parent.name == "Camera Main" || cam.transform.parent.name == "Tilt")) {
+					self.cameras.Add(cam);
+				}
+			});
+		}
+	}
+
+	// EnvironmentDirector sets far clip distance of subcameras instead of main one
+	private static void EnvironmentDirector_FogLogic(ILContext il) {
+		ILCursor cursor = new ILCursor(il).Goto(0);
+
+		// Separate these fucking things and make sure wide angle cam exists before trying to set it
+		if (cursor.TryGotoNext(MoveType.After,
+			x => x.MatchLdarg(0),
+			x => x.MatchLdfld<EnvironmentDirector>("FogStartDistance"),
+			x => x.MatchCall<RenderSettings>("set_fogStartDistance")
+		)) {
+			cursor.Emit(OpCodes.Ldarg_0);
+			cursor.Emit<EnvironmentDirector>(OpCodes.Ldfld, "FogEndDistance");
+			cursor.EmitDelegate((float FogEndDistance) => {
+				if (RenderSettings.fogEndDistance != FogEndDistance)
+					RenderSettings.fogEndDistance = FogEndDistance;
+			});
+		}
+
+		// Add assignment of farClipPlane to wide angle cam's far plane
+		// on environment setup
+		if (cursor.TryGotoNext(MoveType.After,
+			x => x.MatchLdarg(0),
+			x => x.MatchLdfld<EnvironmentDirector>("MainCamera"),
+			x => x.MatchLdarg(0),
+			x => x.MatchLdfld<EnvironmentDirector>("FogEndDistance"),
+			x => x.MatchLdcR4(1),
+			x => x.MatchAdd(),
+			x => x.MatchCallvirt<Camera>("set_farClipPlane")
+		)) {
+			cursor.Emit(OpCodes.Ldarg_0);
+			cursor.Emit<EnvironmentDirector>(OpCodes.Ldfld, "FogEndDistance");
+			cursor.EmitDelegate((float FogEndDistance) => {
+				if (CameraManager.Instance != null) {
+					CameraManager.Instance.FarClipPlane = FogEndDistance + 1f;
+				}
+			});
+		}
+	}
+
+	// Replace assignments to screenSpaceCorners[0-7] with stereographic world-to-viewport-space
+	private static void ValuableDiscoverGraphic_RendererBoundsInScreenSpace(ILContext il) {
+		ILCursor cursor = new ILCursor(il);
+
+		for (int i = 0; i < 8; i++) {
+			// Matches loading a screenSpaceCorners element onto the stack
+			if (cursor.TryGotoNext(MoveType.After,
+				x => x.MatchLdarg(0),
+				x => x.MatchLdfld<ValuableDiscoverGraphic>("screenSpaceCorners"),
+				x => x.MatchLdcI4(i)
+			)) {
+				cursor.RemoveRange(2);
+				cursor.Index += 22; // Seek to after the Vector3 creation
+				cursor.Remove();
+				cursor.EmitDelegate((Vector3 bound) => {
+					return Utility.WorldToViewportPoint(CameraManager.Instance.transform, bound);
+				});
+			}
+		}
+	}
+
+	// Replace the call to WorldToScreenPoint with a wrapper around the stereographic WorldToViewpointPort
+	private static void SemiFunc_OnScreen(ILContext il) {
+		ILCursor cursor = new ILCursor(il);
+
+		if (cursor.TryGotoNext(
+			x => x.MatchLdsfld<CameraUtils>("Instance"),
+			x => x.MatchLdfld<CameraUtils>("MainCamera"),
+			x => x.MatchLdarg(0),
+			x => x.MatchCallvirt<Camera>("WorldToScreenPoint"),
+			x => x.MatchStloc(0)
+		)) {
+			cursor.RemoveRange(4);
+			cursor.Emit(OpCodes.Ldarg_0);
+			cursor.EmitDelegate((Vector3 position) => {
+				Vector3 point = Vector3.zero;
+				if (CameraManager.Instance != null) {
+					point = Utility.WorldToViewportPoint(CameraManager.Instance.transform, position);
+					point.x *= (float)Screen.width;
+					point.y *= (float)Screen.height;
+				} else {
+					point = Camera.main.WorldToScreenPoint(position);
+				}
+				return point;
+			});
+		}
+	}
+}
